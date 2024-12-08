f93398b649dc0fdf4eb9d9cf898ab0fd9f2a3bfe
Prity25-coder <pritynishad480@gmail.com>
2024-12-08 15:34:35 +0530

    Modify description and buy now button

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,10 @@ const products = [
     imageSrc:
       "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg",
     description: "Front of men's Basic Tee in black.",
-    color: "Black",
   },
   {
     id: 2,
     name: "Earthen Bottle",
-
     imageSrc:
       "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg",
     description:
@@ -21,11 +19,18 @@ const products = [
   {
     id: 3,
     name: "Nomad Tumbler",
-    href: "#",
-    description:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
     imageSrc:
       "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    description:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
+  },
+  {
+    id: 3,
+    name: "Nomad Tumbler",
+    imageSrc:
+      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg",
+    description:
+      "Olive drab green insulated bottle with flared screw lid and flat top.",
   },
 ];
 
@@ -50,11 +55,11 @@ function App() {
                   {product.name}
                 </h3>
 
-                <p className="text-m font-medium text-gray-900 mt-2">
+                <p className="description text-m font-medium text-gray-900 mt-2">
                   {product.description}
                 </p>
 
-                <button className=" rounded-lg bg-green-300 px-2 mt-4  hover:bg-green-600">
+                <button className="rounded-sm bg-yellow-600 py-2 mt-4  hover:bg-green-600 w-full">
                   Buy Now
                 </button>
               </div>
